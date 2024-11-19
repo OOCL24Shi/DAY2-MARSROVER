e3189451e7f870fdfd3b8dff3814b391c46f10da
VSHI <36368406@QQ.COM>
2024-11-19 20:57:00 +0800

    Test: add 4 test cases to test turnLeft

--- src/test/java/marsroverTest.java
+++ src/test/java/marsroverTest.java
@@ -9,4 +9,33 @@ public class marsroverTest {
         String report = rover.showStatus();
         assertEquals("0:0:N", report);
     }
+
+    @Test
+    public void should_face_west_when_orient_north_and_turn_left() {
+        MarsRover rover = new MarsRover(0, 0, Direction.N);
+        rover.executeCommands("L");
+        assertEquals("0:0:W", rover.showStatus());
+    }
+
+    @Test
+    public void should_face_north_when_orient_east_and_turn_left() {
+        MarsRover rover = new MarsRover(0, 0,Direction.E);
+        rover.executeCommands("L");
+        assertEquals("0:0:N", rover.showStatus());
+    }
+
+    @Test
+    public void should_face_east_when_orient_south_and_turn_left() {
+        MarsRover rover = new MarsRover(0, 0, Direction.S);
+        rover.executeCommands("L");
+        assertEquals("0:0:E", rover.showStatus());
+    }
+
+    @Test
+    public void should_face_south_when_orient_west_and_turn_left() {
+        MarsRover rover = new MarsRover(0, 0, Direction.W);
+        rover.executeCommands("L");
+        assertEquals("0:0:S", rover.showStatus());
+    }
+
 }
